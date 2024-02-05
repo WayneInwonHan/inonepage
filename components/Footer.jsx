b8f241b7e79b7ff571df2799be172a230bc70f4d
Wayne Han <blissgram@gmail.com>
2024-02-04 23:28:23 -0600

    Add footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,20 @@
+import { FaHeartbeat } from "react-icons/fa";
+
+import Link from "next/link";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="absolute w-full bottom-0 flex justify-center">
+      <div className="flex items-center text-black rounded-s text-[10px] bg-[#fcfbf4] border-2 border-black pr-2 pl-2">
+        &copy; 2024 by{" "}
+        <Link className="font-bold pl-1" href="/">
+          Inwon Han
+        </Link>
+        . Handcrafted with{" "}
+        <FaHeartbeat className="text-red-500 text-[12px] ml-1" />{" "}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
