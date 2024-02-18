b964082b01fdc85e85af8989e52ac2b959e2a5a4
Wayne Han <blissgram@gmail.com>
2024-02-17 23:45:29 -0600

    Add PostCardBar animation

--- components/LatestPostCard.jsx
+++ components/LatestPostCard.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 import Link from "next/link";
+import "./PostCardBar.css";
 
 const LatestPostCard = () => {
   return (
-    <div>
-      <Link href="/blog">Post Title</Link>
+    <div className="w-full">
+      <Link href="/blog">
+        <div className="w-full flex flex-row justify-between">
+          <div className="">this is the recent post title name</div>
+          <div className="PostCardBar">
+            <div id="PostCardBar1"></div>
+            <div id="PostCardBar2"></div>
+            <div id="PostCardBar3"></div>
+            <div id="PostCardBar4"></div>
+            <div id="PostCardBar5"></div>
+            <div id="PostCardBar6"></div>
+            <div id="PostCardBar7"></div>
+            <div id="PostCardBar8"></div>
+            <div id="PostCardBar9"></div>
+            <div id="PostCardBar10"></div>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
