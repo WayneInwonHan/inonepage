cd67a9fca5bae46f485d57ef54cf7167a00f28a8
Wayne Han <blissgram@gmail.com>
2024-03-05 23:16:52 -0600

    change about image setting

--- components/AboutImage.jsx
+++ components/AboutImage.jsx
@@ -7,7 +7,6 @@ const AboutImage = ({ imgSrc, backImgSrc }) => {
   const containerRef = useRef(null);
   const [isFlipped, setIsFlipped] = useState(false);
 
-  // Define default styles for the overlay to be visible initially
   const defaultOverlayStyle = {
     filter: "opacity(0)",
     backgroundPosition: "100%",
@@ -22,23 +21,30 @@ const AboutImage = ({ imgSrc, backImgSrc }) => {
     const container = containerRef.current;
 
     const handleMouseMove = (e) => {
-      const { offsetX: x, offsetY: y } = e;
-      const rotateY = (-1 / 5) * x + 20;
-      const rotateX = (4 / 30) * y - 20;
+      const bounds = container.getBoundingClientRect();
+      const x = e.clientX - bounds.left - bounds.width / 2;
+      const y = e.clientY - bounds.top - bounds.height / 2;
+
+      // Scale down the effect so it's less intense and more balanced
+      const scale = 20; // Adjust this value to control the effect intensity
+      const rotateY = (x / bounds.width) * scale;
+      const rotateX = -(y / bounds.height) * scale;
 
       setOverlayStyle({
         ...defaultOverlayStyle,
-        backgroundPosition: `${x / 5 + y / 5}%`,
-        filter: `opacity(${x / 400}) brightness(0.5)`,
+        backgroundPosition: `${50 + (x / bounds.width) * 50}% ${
+          50 + (y / bounds.height) * 50
+        }%`,
+        filter: `opacity(${Math.abs(x / bounds.width)}) brightness(0.5)`,
       });
 
-      container.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      container.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
     };
 
     const handleMouseOut = () => {
       setOverlayStyle(defaultOverlayStyle);
       container.style.transform =
-        "perspective(1000px) rotateY(0deg) rotateX(0deg)";
+        "perspective(500px) rotateY(0deg) rotateX(0deg)";
     };
 
     container.addEventListener("mousemove", handleMouseMove);
@@ -54,12 +60,16 @@ const AboutImage = ({ imgSrc, backImgSrc }) => {
     <div className="flex flex-col items-center">
       <div
         ref={containerRef}
-        className={`relative w-[400px] h-[400px] rounded-3xl overflow-hidden transition-transform duration-700 ease-linear ${
+        className={`relative w-[450px] h-[450px] rounded-3xl overflow-hidden transition-transform duration-700 ease-linear ${
           isFlipped ? "rotate-y-180" : ""
         }`}
       >
         <div
-          className="absolute inset-0 bg-gradient-to-r from-transparent via-yellow-200 to-purple-500 z-10 mix-blend-color-dodge bg-100% transition-all duration-100 ease-linear"
+          className="absolute inset-0 bg-gradient-to-r from-transparent via-yellow-200 to-purple-500 z-10 mix-blend-color-dodge bg-[linear-gradient(45deg,
+    transparent 40%,
+    rgba(255, 219, 112, 0.8) 45%,
+    rgba(132, 50, 255 ,0.6) 50%,
+    transparent 54%)] transition-all duration-100 ease-linear"
           style={overlayStyle}
         />
         <Image
