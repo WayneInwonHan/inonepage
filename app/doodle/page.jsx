d4a7bfa607de3b616445160b5bac93ca331e6f5b
Wayne Han <blissgram@gmail.com>
2024-03-19 11:26:06 -0500

    change doodle popup UI

--- app/doodle/page.jsx
+++ app/doodle/page.jsx
@@ -1,29 +1,48 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import doodleData from "../../lib/doodleData";
+import doodleData from "../../lib/doodleData"; // Ensure this path is correct for your project structure
+
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 const Doodle = () => {
   const [selectedImg, setSelectedImg] = useState(null);
 
+  // Function to open modal and display the selected image
   const handleOpenModal = (index) => {
     setSelectedImg(index);
   };
 
+  // Function to close the modal
   const handleCloseModal = () => {
     setSelectedImg(null);
   };
 
+  // Function to navigate to the next image
   const handleNext = () => {
     setSelectedImg((prevIndex) => (prevIndex + 1) % doodleData.length);
   };
 
+  // Function to navigate to the previous image
   const handlePrev = () => {
     setSelectedImg(
       (prevIndex) => (prevIndex - 1 + doodleData.length) % doodleData.length,
     );
   };
+
+  // Effect to lock the body scroll when the modal is open, preventing background scroll
+  useEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    if (selectedImg !== null) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = originalStyle;
+    }
+
+    // Cleanup function to reset the overflow style
+    return () => (document.body.style.overflow = originalStyle);
+  }, [selectedImg]);
 
   return (
     <div className="w-full h-full gap-6 flex flex-col">
@@ -36,7 +55,7 @@ const Doodle = () => {
               <div className="page-window-bar-button"></div>
             </div>
           </div>
-          <div className="page-window-content p-5 justify-center align-center bg-gradient-to-r from-[#2D004B] to-black">
+          <div className="page-window-content p-5 justify-center align-center bg-gradient-to-r from-[#2D004B] to-black overflow-y-scroll">
             <div className="w-full h-full flex justify-center items-center">
               <div className="noise-effect"></div>
               <div className="w-full h-full p-5 items-center">
@@ -44,7 +63,7 @@ const Doodle = () => {
                   {doodleData.map((doodle, index) => (
                     <div
                       key={index}
-                      className="relative w-full h-[200px] max-w-[300px] border-2 border-black rounded-sm cursor-pointer group overflow-hidden"
+                      className="relative w-full h-[200px] max-w-[300px] border-2 border-black rounded-sm cursor-pointer group overflow-hidden z-10"
                       onClick={() => handleOpenModal(index)}
                     >
                       <Image
@@ -55,18 +74,21 @@ const Doodle = () => {
                         className="transition-all duration-500"
                       />
                       <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-0 group-hover:opacity-20 transition-opacity duration-500"></div>
-                      <h2 className="absolute bottom-0 left-0 w-full text-white text-center opacity-0 group-hover:opacity-100 transition-opacity duration-500 z-[2]">
+                      <h2 className="absolute bottom-0 left-0 w-full text-white text-left pl-2 font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-500 z-[2]">
                         {doodle.title}
                       </h2>
                     </div>
                   ))}
                 </div>
                 {selectedImg !== null && (
-                  <div className="fixed w-full h-full inset-0 bg-black bg-opacity-50 z-50 flex flex-col justify-center items-center">
-                    <div className="flex flex-row">
-                      <button className="bg-white" onClick={handlePrev}>
-                        Prev
-                      </button>
+                  <div
+                    className="absolute inset-0 bg-black bg-opacity-50 z-20 flex justify-center items-center cursor-pointer"
+                    onClick={handleCloseModal}
+                  >
+                    <div
+                      className="p-5 rounded-lg flex flex-col items-center"
+                      onClick={(e) => e.stopPropagation()}
+                    >
                       <Image
                         src={doodleData[selectedImg].image}
                         alt="Selected Doodle"
@@ -74,14 +96,26 @@ const Doodle = () => {
                         height={500}
                         priority
                       />
-                      <button className="bg-white" onClick={handleNext}>
-                        Next
-                      </button>
-                    </div>
-                    <div className="flex justify-center">
-                      <button className="bg-white" onClick={handleCloseModal}>
-                        Close
-                      </button>
+                      <div className="flex justify-between items-center w-full mt-4">
+                        <button
+                          className="bg-gray-300 hover:bg-gray-400 text-black font-bold w-[50px] h-[50px] flex justify-center items-center rounded-[100%] transition-all duration-300"
+                          onClick={handlePrev}
+                        >
+                          <FaArrowLeft className="text-[1.25rem]" />
+                        </button>
+                        <button
+                          className="bg-red-500 hover:bg-red-700 text-white font-bold w-[50px] h-[50px] flex justify-center items-center rounded-[100%] transition-all duration-300"
+                          onClick={handleCloseModal}
+                        >
+                          X
+                        </button>
+                        <button
+                          className="bg-gray-300 hover:bg-gray-400 text-black font-bold w-[50px] h-[50px] flex justify-center items-center rounded-[100%] transition-all duration-300"
+                          onClick={handleNext}
+                        >
+                          <FaArrowRight className="text-[1.25rem]" />
+                        </button>
+                      </div>
                     </div>
                   </div>
                 )}
